626e650a0c7972f048680ed386d5acfafc5bb52e
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-11-01 22:23:43 +0900

    cal practice

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,10 @@
-A = int(1900)
-if((A%4)==0&((A%100)!=0|(A%400)==0)):
-    print("1")
-else:
-    print("0")
+N = int(5)
+A = (20, 10, 35, 30, 7)
+M=A[0]
+m =A[0]
+for i in range(N):
+    if A[i]>M:
+        M= A[i]
+    elif A[i]<m:
+        m= A[i]
+print(M,m)
